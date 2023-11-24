9de872d3c63d7bb05053e92d924558ea620a3e1f
carrena <carrena@feelanet.com>
2023-11-24 12:59:44 +0900

    c

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var v1Api = app.MapGroup("/api/v1");
+v1Api.MapGet("/getutcnow", () => DateTime.Now.ToString());
+
+var v2Api = app.MapGroup("/api/v2");
+v2Api.MapGet("/getnow", () => DateTime.Now.ToString());
+
+app.Run();
+ 
